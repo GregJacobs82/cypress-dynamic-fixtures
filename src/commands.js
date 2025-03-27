62e49a5a1a2f11c50e80ca51e5809b2e839cd984
Greg <Greg@2book.com>
2025-03-27 14:29:41 -0400

    remove double-quotes

--- src/commands.js
+++ src/commands.js
@@ -1,22 +1,22 @@
-Cypress.Commands.overwrite("fixture", (originalFn, filePath, encoding, options) => {
-    if (filePath.endsWith(".js")) {
+Cypress.Commands.overwrite('fixture', (originalFn, filePath, encoding, options) => {
+    if (filePath.endsWith('.js')) {
         // The plugin stored all our fixture data here
-        const allJsFixtures = Cypress.env("__ALL_JS_FIXTURES__") || {};
+        const allJsFixtures = Cypress.env('__ALL_JS_FIXTURES__') || {};
 
         // Optional: some debug logging
-        const isDebug = process.env.NODE_ENV !== "production";
+        const isDebug = process.env.NODE_ENV !== 'production';
         if (isDebug) {
-            console.log("[dynamic-fixtures] filePath =>", filePath);
-            console.log("[dynamic-fixtures] allJsFixtures keys =>", Object.keys(allJsFixtures));
+            console.log('[dynamic-fixtures] filePath =>', filePath);
+            console.log('[dynamic-fixtures] allJsFixtures keys =>', Object.keys(allJsFixtures));
         }
 
         // Transform file path the same way we did in plugin.js
-        let noExt = filePath.replace(/\.js$/, "");
-        let replaced = noExt.replace(/\//g, "$");
+        let noExt = filePath.replace(/\.js$/, '');
+        let replaced = noExt.replace(/\//g, '$');
         let transformedKey = replaced.replace(/-([a-zA-Z])/g, (_, c) => c.toUpperCase());
 
         if (isDebug) {
-            console.log("[dynamic-fixtures] transformedKey =>", transformedKey);
+            console.log('[dynamic-fixtures] transformedKey =>', transformedKey);
         }
 
         // Return the matching object from the dictionary
