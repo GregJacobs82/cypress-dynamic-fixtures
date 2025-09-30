134b8e5cc87c27be80b74c1b69a470fb12a309b7
Greg <Greg@2book.com>
2025-09-30 16:08:40 -0400

    20250930 add tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-const { dynamicFixturePlugin } = require('./plugin'); // Re-export the plugin so user can import it easily
 require('./commands'); // commands.js overwrites `cy.fixture`
-
-module.exports = {
-    dynamicFixturePlugin
-};
+module.exports = {};
